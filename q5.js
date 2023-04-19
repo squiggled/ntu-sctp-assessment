eeb9c2c4c155b4ce8c69b85e6f8d9b824fac2d0a
squiggled <98679985+squiggled@users.noreply.github.com>
2023-04-19 20:31:39 +0800

    Add files via upload

---
+++ q5.js
@@ -0,0 +1,13 @@
+
+/*
+    Task 1:
+    - Import the file `external.js`.
+    - Destructure the imported function from `external.js` to display the string
+      "I am printing something" on console.log.
+*/
+
+// Task 1: Add code here
+import {print} from `./external.js`;
+
+// Do not change the code below
+print();
